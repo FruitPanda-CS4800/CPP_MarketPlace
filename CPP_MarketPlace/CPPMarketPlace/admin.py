8b6b0744bf39972fe45e3158a2f946800dda027a
Eric Chen <eschen@cpp.edu>
2023-03-01 19:35:52 -0800

    implement function with import_export library

--- CPP_MarketPlace/CPPMarketPlace/admin.py
+++ CPP_MarketPlace/CPPMarketPlace/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
+from . import models
 
 # Register your models here.
+
+#Add import and export functionality in admin page for Product database table
+class Products(ImportExportModelAdmin, admin.ModelAdmin):
+    ...
+admin.site.register(models.Product, Products)
