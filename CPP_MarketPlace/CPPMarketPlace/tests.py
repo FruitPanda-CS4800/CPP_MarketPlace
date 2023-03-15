d51fcc9182efa823b65039108a62279cd7ec9394
Eric Chen <eschen@cpp.edu>
2023-03-15 16:28:20 -0700

    add unit test for adding Product to model

--- CPP_MarketPlace/CPPMarketPlace/tests.py
+++ CPP_MarketPlace/CPPMarketPlace/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import Product
 
-# Create your tests here.
+class ProductModelTestCase(TestCase):
+    def setUp(self):
+        self.product = Product.objects.create(
+            name='Test Product',
+            category='Test Category'
+        )
+    
+    def test_product_creation(self):
+        """Test that a product is created with the correct attributes"""
+        self.assertEqual(self.product.name, 'Test Product')
+        self.assertEqual(self.product.category, 'Test Category')
+        self.assertEqual(self.product.image.name, '')
+        self.assertIsNotNone(self.product.created)
