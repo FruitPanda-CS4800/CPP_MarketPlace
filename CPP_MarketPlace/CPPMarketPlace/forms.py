0af320784229e40b0607311153fe4a7b226bea29
Eric Chen <eschen@cpp.edu>
2023-03-18 14:04:23 -0700

    update create product form

--- CPP_MarketPlace/CPPMarketPlace/forms.py
+++ CPP_MarketPlace/CPPMarketPlace/forms.py
@@ -7,4 +7,4 @@ class SearchForm(forms.Form):
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        fields = ['name', 'category', 'image']
+        fields = ['name', 'description', 'price', 'category', 'image']
