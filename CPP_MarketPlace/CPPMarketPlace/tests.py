8a77e3069069332a88aafc3f51728c6392486334
Grecia Alvarado <greciaalvarado68@gmail.com>
2023-03-17 21:26:14 -0700

    Added successful and failure unit tests

--- CPP_MarketPlace/CPPMarketPlace/tests.py
+++ CPP_MarketPlace/CPPMarketPlace/tests.py
@@ -14,3 +14,26 @@ class ProductModelTestCase(TestCase):
         self.assertEqual(self.product.category, 'Test Category')
         self.assertEqual(self.product.image.name, '')
         self.assertIsNotNone(self.product.created)
+
+
+class ProductModelTestCase2(TestCase):
+    def setUp(self):
+        self.product = Product.objects.create(
+            name='Calculus II',
+            category='Textbooks'
+        )
+    
+    def test_product_creationCorrect(self):
+        """Test that a product is created with the correct attributes"""
+        self.assertEqual(self.product.name, 'Calculus II')
+        self.assertEqual(self.product.category, 'Textbooks')
+        self.assertEqual(self.product.image.name, '')
+        self.assertIsNotNone(self.product.created)
+
+    def test_product_creationWrong(self):
+        """Test that a product is created with the correct attributes"""
+        self.assertEqual(self.product.name, 'Math')
+        self.assertEqual(self.product.category, 'Books')
+        self.assertEqual(self.product.image.name, '')
+        self.assertIsNotNone(self.product.created)
+
