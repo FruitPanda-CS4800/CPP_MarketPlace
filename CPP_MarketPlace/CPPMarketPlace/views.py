f2dedab7b87af4192ec1ce5386ac1e817f890aae
Eric Chen <eschen@cpp.edu>
2023-05-01 22:01:09 -0700

    add styling to product page & user account db table

--- CPP_MarketPlace/CPPMarketPlace/views.py
+++ CPP_MarketPlace/CPPMarketPlace/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from CPPMarketPlace.models import Product
+from CPPMarketPlace.models import Product, UserProfile
 from CPPMarketPlace.serializers import ProductSerializer
 from django.contrib.auth.decorators import login_required
 from .forms import ProductForm
-
+from register import views as register
 #from serializers import ProductSerializer
 # Create your views here.
 
@@ -45,20 +45,28 @@ def search(request):
     return render(request, 'search_results.html', {'results': results , 'query':productName})
 
 def create_product(request):
-    if request.method == 'POST':
-        form = ProductForm(request.POST, request.FILES)
-        if form.is_valid():
-            product = form.save()
-            print("Added")
-            return render(request, 'create_product.html', {'form': form, 'success': True})
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            form = ProductForm(request.POST, request.FILES)
+            if form.is_valid():
+                user = request.user
+                 # create a new product instance with the user as the owner
+                product = form.save(commit=False)
+                product.owner = user
+                product.save()
+                print("Added")
+                return render(request, 'create_product.html', {'form': form, 'success': True})
+            else:
+                print("Did not add")
+                return render(request, 'create_product.html', {'form': form, 'error_message': 'Product was not added.'})
         else:
-            print("Did not add")
-            return render(request, 'create_product.html', {'form': form, 'error_message': 'Product was not added.'})
+            form = ProductForm()
+        return render(request, 'create_product.html', {'form': form})
     else:
-        form = ProductForm()
-    return render(request, 'create_product.html', {'form': form})
+        return register.loginPage(request)
 
 def product_detail(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
-    context = {'product': product}
+    user_profile = UserProfile.objects.get(user=product.owner)
+    context = {'product': product, 'user_profile': user_profile}
     return render(request, 'product_page.html', context)
